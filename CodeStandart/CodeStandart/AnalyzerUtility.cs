83ffb7c7c0e05222335f03b20b4b5fb12e217905
KAnon <mr.kusya@gmail.com>
2019-07-10 15:40:33 +0300

    Добавлена AnalyzerUtility

---
+++ CodeStandart/CodeStandart/AnalyzerUtility.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace CodeStandart
+{
+    static internal class AnalyzerUtility
+    {
+        public static LocalizableResourceString GetLocalizedString(string id, LocalizedStringKind kind)
+        {
+            string name = "";
+
+            switch (kind)
+            {
+                case LocalizedStringKind.Title:
+                    name = id + "_AnalyzerTitle";
+                    break;
+                case LocalizedStringKind.FormattableMessage:
+                    name = id + "_AnalyzerMessageFormat";
+                    break;
+                case LocalizedStringKind.Description:
+                    name = id + "_AnalyzerDescription";
+                    break;
+            }
+
+            return new LocalizableResourceString(name, Resources.ResourceManager, typeof(Resources));
+        }
+
+        public static DiagnosticDescriptor CreateDiagnosticDescriptor(string id, string category, DiagnosticSeverity severity)
+        {
+            var title = GetLocalizedString(id, LocalizedStringKind.Title);
+            var message = GetLocalizedString(id, LocalizedStringKind.FormattableMessage);
+            var description = GetLocalizedString(id, LocalizedStringKind.Description);
+
+            return new DiagnosticDescriptor(id, title, message, category, severity, true, description);
+        }
+    }
+
+    internal enum LocalizedStringKind
+    {
+        Title,
+        FormattableMessage,
+        Description
+    }
+}
