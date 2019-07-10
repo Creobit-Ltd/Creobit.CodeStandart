98f1d8fa7d19e4797d8ce424732ec05fd1c33184
KAnon <mr.kusya@gmail.com>
2019-07-10 18:12:32 +0300

    Заготовка под автоматическое исправление отсутствия пустой строки перед операторами управления

---
+++ CodeStandart/CodeStandart/CodeFixProviders/CREO004_NewLineBeforeDirectivesCodeFixProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.CSharp.CodeStyle;
+using Microsoft.CodeAnalysis.CodeStyle;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace CodeStandart.CodeFixProviders
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = "CREO004_NewLineBeforeDirectives"), Shared]
+    public class NewLineBeforeDirectivesCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+            => ImmutableArray.Create(CREO005_NewLineAfterUsingBlockAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        private const string Title = "Вставить пустую строку перед оператором управления";
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var statement = root.FindToken(diagnosticSpan.Start)
+                .Parent
+                    .AncestorsAndSelf()
+                        .OfType<StatementSyntax>()
+                        .First();
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: Title,
+                    createChangedDocument: c => AddNewLineBefore(context.Document, statement, c),
+                    equivalenceKey: Title),
+                diagnostic);
+        }
+
+        private async Task<Document> AddNewLineBefore(Document document, StatementSyntax statement, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            return document;
+        }
+    }
+}
